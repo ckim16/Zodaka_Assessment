e34450c2d6dfbcaae1d73c5620c7fd5bb381f64e
ckim16 <aero.cheewoon@gmail.com>
2018-01-21 12:13:56 -0800

    create search option by language

--- client/src/containers/tweets.js
+++ client/src/containers/tweets.js
@@ -5,6 +5,12 @@ import { TwitterTweetEmbed } from 'react-twitter-embed';
 class Tweets extends Component {
   constructor(props) {
     super(props);
+
+    this.state = { option: '' };
+  }
+
+  _onOptionChange(option) {
+    this.setState({ option });
   }
 
   searchTerm() {
@@ -16,16 +22,56 @@ class Tweets extends Component {
     }
   }
 
+  searchOptions() {
+    if (this.props.tweets) {
+      let all_class = (this.state.option === 'EN') ? 'btn btn-secondary' : 'btn btn-secondary active';
+      let en_class = (this.state.option === 'EN') ? 'btn btn-secondary active' : 'btn btn-secondary'; 
+      return (
+        <div className="btn-group btn-group-toggle" data-toggle="buttons">
+          <label className={all_class}>
+            <input 
+              type="radio"
+              onClick={this._onOptionChange.bind(this, 'All')}
+              autoComplete="off" 
+              checked={this.state.option === 'All'}
+            />All Tweets
+          </label>
+          <label className={en_class}>
+            <input 
+              type="radio"
+              onClick={this._onOptionChange.bind(this, 'EN')} 
+              autoComplete="off" 
+              checked={this.state.option === 'EN'}
+            />EN Tweets Only
+          </label>
+        </div>
+      );
+    }
+  }
+
   renderTweet() {
     if (this.props.tweets) {
-      return this.props.tweets.tweets.map((t, idx) => {
-        return (
-          <TwitterTweetEmbed
-            key={idx}
-            tweetId={t.id_str}
-          />
-        );
-      })
+      if (this.state.option === 'EN') {
+        return this.props.tweets.tweets.map((t, idx) => {
+          if (t.lang === 'en') {
+            return (
+              <TwitterTweetEmbed
+                key={idx}
+                tweetId={t.id_str}
+              />
+            );
+          }
+        });
+      } else {
+        return this.props.tweets.tweets.map((t, idx) => {
+          return (
+            <TwitterTweetEmbed
+              key={idx}
+              tweetId={t.id_str}
+            />
+          );
+        });
+      }
     }
   }
 
@@ -33,6 +79,7 @@ class Tweets extends Component {
     return (
       <div className="tweets">
         {this.searchTerm()}
+        {this.searchOptions()}
         {this.renderTweet()}
       </div>
     );
