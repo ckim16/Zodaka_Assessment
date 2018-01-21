386dd1d484db57ac836dac880639ac1477975ba5
ckim16 <aero.cheewoon@gmail.com>
2018-01-21 11:13:36 -0800

    import components

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+import Header from './header';
 import SearchBox from '../containers/search_box';
 import Tweets from '../containers/tweets';
 
@@ -7,6 +8,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
+        <Header/>
         <SearchBox/>
         <Tweets/>
       </div>
