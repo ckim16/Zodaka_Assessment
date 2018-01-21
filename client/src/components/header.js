66395215ee7927e4a866f0fa716b8983a733497d
ckim16 <aero.cheewoon@gmail.com>
2018-01-21 11:12:40 -0800

    create header file

---
+++ client/src/components/header.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onClickLinkedin = this.onClickLinkedin.bind(this);
+    this.onClickGithub = this.onClickGithub.bind(this);
+  }
+
+  onClickLinkedin() {
+    window.open('https://www.linkedin.com/in/cheewoonkim');
+  }
+
+  onClickGithub() {
+    window.open('https://www.github.com/ckim16');
+  }
+
+  render() {
+    return (
+      <div className="jumbotron">
+        <h1>Zodaka Technical Assessment</h1>
+        <p>Zodaka Technical Assignment from Cheewoon Kim</p>      
+        <div className="right">
+          <label>Linkedin: <i className="fa fa-linkedin-square fa-2x" aria-hidden="true" onClick={this.onClickLinkedin}></i></label>
+          <label>Github: <i className="fa fa-github-square fa-2x" aria-hidden="true" onClick={this.onClickGithub}></i></label>
+        </div>
+      </div>
+    );
+  }
+}
