d3bf5027b060a51fe833ccfb170ede47ec91f2a2
ckim16 <aero.cheewoon@gmail.com>
2018-01-22 00:12:46 -0800

    add comment

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -6,13 +6,14 @@ const BASE_URL = 'http://localhost:3000';
 
 export function searchTweets(key) {
   return function(dispatch) {
-    axios.post(`${BASE_URL}/search/${key}`, { key })
+    axios.post(`${BASE_URL}/search/${key}`, { key }) // post request to server
     .then(response => {
+      // dispatch your response with type and payload (redux-thunk)
       dispatch({ 
         type: SEARCH_TWEET, 
         payload: { tweets: response.data.statuses, query: response.data.search_metadata.query } 
       });
     })
-    .catch(err => console.log('search tweets err', err));
+    .catch(err => console.log('search tweets err', err)); // error handling
   };
 }
