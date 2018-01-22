3a43fc196e086ca98c4e2f0c76764c79d8865ac2
ckim16 <aero.cheewoon@gmail.com>
2018-01-22 00:28:10 -0800

    add comment

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  entry: './client/src/index.js',
-  output: {
+  entry: './client/src/index.js', // entry file for webpack
+  output: { // output filename with path
     path: __dirname,
     filename: 'bundle.js'
   },
-  module: {
+  module: { // module that needs to be tested when compiled
     loaders: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ }, 
       { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
     ]
   },
-  devServer: {
+  devServer: { // serve client folder
     contentBase: './client'
   }
 };
